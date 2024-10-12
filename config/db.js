8bd1462cd378dea9ab433d172c395f806f11d29e
mohamedkhalil9 <mohammedkhalil098@gmail.com>
2024-10-13 00:23:31 +0300

    database connection string using environment variables

--- config/db.js
+++ config/db.js
@@ -1,10 +1,8 @@
 import mongoose from 'mongoose';
 
-const DB_URI = 'mongodb+srv://zmohamedkhalil9:STtRtthsVdPd5yLH@cluster0.i40eg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-
 const dbConnect = () => {
   mongoose
-    .connect(DB_URI)
+    .connect(process.env.DB_URI)
     .then(console.log('db connected'))
     .catch((err) => {
       console.log(err)
