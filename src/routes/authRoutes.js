1cd67c32d9c47b2c3fcf1bbc25363dfba4b20afa
Mohamed Khalil <mohamedkhalildev@gmail.com>
2024-10-12 18:21:18 +0300

    test

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,7 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/register', (req, res) => {
-  res.status(200).json({ msg: "register route" });
+  res.status(200).json({ success: true, msg: "register route" });
 });
 
 export default router;
