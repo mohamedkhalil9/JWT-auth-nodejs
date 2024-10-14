8cafe060a3ef7d551593c91e707f7cb63768aa66
Mohamed Khalil <mohamedkhalildev@gmail.com>
2024-10-14 14:16:16 +0300

    error handling and get users routes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,16 +1,17 @@
 import express from 'express';
 import { User } from '../models/userModel.js';
+import asyncWrapper from '../utils/asyncWrapper.js';
 
 const router = express.Router();
 
-router.post('/register', async (req, res) => {
+router.post('/register', asyncWrapper(async (req, res) => {
   const { name, email, password } = req.body;
   if ( !name || !email || !password ) {
     return res.json({ msg: "enter all fields"});
   }
   await User.create({ name, email, password });
   res.status(201).json({ success: true, msg: "new user created" });
-});
+}))
 
 
 export default router;
--- src/utils/asyncWrapper.js
+++ src/utils/asyncWrapper.js
@@ -1,17 +1,9 @@
 const asyncWrapper = (fn) => {
-  return async (req, res, next) => {
-    try {
-      await fn(req, res, next);
-    } catch (error) {
-      next(error); // Passes error to error handling middleware
-    }
-  };
-};
-
-module.exports = (asyncFn) => {
     return (req, res, next) => {
-        asyncFn(req, res, next).catch((err) => {
+        fn(req, res, next).catch((err) => {
             next(err);
         });
     }
 }
+
+export default asyncWrapper;
