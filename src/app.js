17e7a9af5177bf9279e873a6775c7e85df1a4c15
Mohamed Khalil <mohamedkhalildev@gmail.com>
2024-10-14 16:48:10 +0300

    global 404 middleware and global error handler

--- src/app.js
+++ src/app.js
@@ -11,4 +11,13 @@ dbConnect();
 
 app.use('/api/v1', appRoutes);
 
+app.all('*', (req, res, next)=> {
+    return res.status(404).json({ status: "error", message: 'this resource is not available'});
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.statusCode || 500).json({ status: error.statusText || "error", message: error.message, code: error.statusCode || 500, data: null});
+})
+
+
 export default app;
