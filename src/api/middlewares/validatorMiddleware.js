b493e7294fa2b4b1d99915ad57c7261ab719e6af
mohamedkhalil9 <mohammedkhalil098@gmail.com>
2024-10-18 14:32:28 +0300

    adding update user data and password

--- src/api/middlewares/validatorMiddleware.js
+++ src/api/middlewares/validatorMiddleware.js
@@ -2,6 +2,6 @@ import { validationResult } from 'express-validator';
 
 export const validatorMiddleware = (req, res, next) => {
   const errors = validationResult(req);
-  if (!errors.isEmpty()) return res.status(400).json({errors: errors.array()});
+  if (!errors.isEmpty()) return res.status(400).json({status: "error", message: errors.array()});
   next();
 }
--- src/api/routes/userRoutes.js
+++ src/api/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getUsers, getUser, deleteUser, getCurrentUser } from '../controllers/userController.js';
+import { getUsers, getUser, deleteUser, updateUser, getCurrentUser } from '../controllers/userController.js';
 import verifyToken from '../middlewares/verifyToken.js'
 import allowedTo from '../middlewares/allowedTo.js'
 import { idValidator } from '../validation/validators.js';
@@ -12,5 +12,6 @@ router.route('/').get(verifyToken, allowedTo('manager', 'admin'), getUsers)
 router.route('/:id')
   .get(verifyToken, allowedTo('manager', 'admin'), idValidator, getUser)
   .delete(verifyToken, allowedTo('admin'), idValidator, deleteUser)
+  .patch(verifyToken, allowedTo('admin'), idValidator, updateUser)
 
 export default router;
