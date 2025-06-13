f4c1175b672635bd91bb93635faa71e978c12e03
Mohamed Khalil <mohamedkhalildev@gmail.com>
2025-06-13 21:52:35 +0300

    Add: cloudinary image cloud

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
 import http from "http";
 import app from "./app.js";
 import dbConnect from "./config/db.js";
+import connectCloudinary from "./config/cloudinary.js";
 
 const PORT = process.env.PORT;
 
 const server = http.createServer(app);
 
 dbConnect();
+connectCloudinary();
 
 // TODO: process.exit() ?
 server.listen(PORT, () => {
