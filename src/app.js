c9e18a8699efa22d8b1ff40b4c7e505e01bc03ca
Mohamed Khalil <mohamedkhalildev@gmail.com>
2025-06-27 11:13:11 +0300

    Add: security packages

--- src/app.js
+++ src/app.js
@@ -1,32 +1,54 @@
 import express from "express";
 import "dotenv/config";
-import cookieParser from "cookie-parser";
-import passport from "passport";
-import cors from "cors";
-import helmet from "helmet";
 import morgan from "morgan";
 import rateLimit from "express-rate-limit";
+import helmet from "helmet";
+import compression from "compression";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import mongoSanitize from "express-mongo-sanitize";
+import xss from "xss-clean";
+import hpp from "hpp";
+import passport from "passport";
 import { notFound, globalErrorHandler } from "./middlewares/errorHandler.js";
 import appRouter from "./routes/index.js";
 
-const app = express();
-app.use(express.json());
-app.use(cookieParser());
+import swaggerUi from "swagger-ui-express";
+import YAML from "js-yaml";
+import fs from "fs";
 
+const yamlFile = fs.readFileSync("src/utils/swagger.yaml", "utf8");
+const swaggerDocument = YAML.load(yamlFile);
+
+// dev and prod ENVs
+// automation testing
+const app = express();
+
+app.use(morgan("dev"));
 const apiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // Limit each IP to 100 requests per windowMs
 });
 app.use("/api", apiLimiter);
+
 app.use(helmet());
+app.use(compression());
 app.use(
   cors({
     credentials: true,
   }),
 );
-app.use(morgan("dev"));
+app.use(express.json({ limit: "50kb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+app.use(mongoSanitize());
+app.use(xss());
+app.use(hpp());
 
 app.use(passport.initialize());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/api/v1", appRouter);
 
 app.all("*", notFound);
